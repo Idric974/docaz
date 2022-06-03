8df0fe8bd607e6fbe288200b46a9ab81c26a4f77
Idric Evarne <68675330+Idric974@users.noreply.github.com>
2022-06-03 22:12:51 +0400

    Mise à jour condition si user est connecté

--- src/components/User/SingnIndex.js
+++ src/components/User/SingnIndex.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../../../styles/SingnIndex.module.css';
-import Logo from '../Logo/Logo';
+import { useSelector } from 'react-redux';
+import ReadProfil from './ReadProfil';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 
 const SingnIndex = (props) => {
   const [signUpModal, setSignUpModal] = useState(props.signup);
   const [signInModal, setSignInModal] = useState(props.signin);
+  const [readProfilModal, setReadProfilModal] = useState(props.readProfil);
+
+  //
+  //! I) Affichage des informations concernant l'utilisateur connecté.
+
+  //? I) Récupéra Le profile de l'utilisateur connecté.
+
+  const userData = useSelector((state) => state.userCRUDReducer);
+  console.log('userData', userData);
+
+  //? --------------------------------------------------
 
   useEffect(() => {
-    setSignInModal(true);
-  }, []);
+    if (userData) {
+      setReadProfilModal(true);
+    } else {
+      setSignUpModal(true);
+    }
+  }, [userData]);
 
   const handleModals = (e) => {
     if (e.target.id === 'register') {
@@ -23,24 +39,29 @@ const SingnIndex = (props) => {
   };
   return (
     <div className={styles.box}>
-      <Logo></Logo>
-      <div className={styles.boxTab}>
-        <div
-          className={signUpModal ? styles.tab : null}
-          onClick={handleModals}
-          id="register"
-        >
-          {"S'inscrire"}
+      {userData ? (
+        <div className={styles.profileBox}>
+          {readProfilModal && <ReadProfil />}
         </div>
+      ) : (
+        <div className={styles.boxTab}>
+          <div
+            className={signUpModal ? styles.tab : null}
+            onClick={handleModals}
+            id="register"
+          >
+            {"S'inscrire"}
+          </div>
 
-        <div
-          className={signInModal ? styles.tab : null}
-          onClick={handleModals}
-          id="login"
-        >
-          {'Se connecter'}
+          <div
+            className={signInModal ? styles.tab : null}
+            onClick={handleModals}
+            id="login"
+          >
+            {'Se connecter'}
+          </div>
         </div>
-      </div>
+      )}
 
       {signUpModal && <SignUp />}
       {signInModal && <SignIn />}
